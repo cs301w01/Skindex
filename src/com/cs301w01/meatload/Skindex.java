c874361e73d06439c36e857483b45769560301e1
Joel Burford <joelburford@gmail.com>
2012-03-12 15:27:03 -0600

    Project builds, tab main page works

--- src/com/cs301w01/meatload/Skindex.java
+++ src/com/cs301w01/meatload/Skindex.java
@@ -1,16 +1,24 @@
 package com.cs301w01.meatload;
 
 import android.app.Activity;
+import android.app.TabActivity;
+import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TabHost;
 
 import com.cs301w01.meatload.activities.Skindactivity;
+import com.cs301w01.meatload.activities.ViewGroupsActivity;
+import com.cs301w01.meatload.activities.ViewTagsActivity;
 import com.cs301w01.meatload.controllers.PhotoManager;
 
 import java.util.Collection;
 
-public class Skindex extends Skindactivity
+public class Skindex extends TabActivity
+
+//used to extend Skindactivity
 {
 	private Button newAlbumButton;
 	private Button takePictureButton;
@@ -22,13 +30,37 @@ public class Skindex extends Skindactivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        
+        Resources res = getResources(); // Resource object to get Drawables
+        TabHost tabHost = getTabHost();  // The activity TabHost
+        TabHost.TabSpec spec;  // Resusable TabSpec for each tab
+        Intent intent;  // Reusable Intent for each tab
+
+        // Create an Intent to launch an Activity for the tab (to be reused)
+        intent = new Intent().setClass(this, ViewGroupsActivity.class);
+
+        // Initialize a TabSpec for each tab and add it to the TabHost
+        spec = tabHost.newTabSpec("albums").setIndicator("Albums",
+                           res.getDrawable(R.drawable.ic_tab_artists))
+                         .setContent(intent);
+        tabHost.addTab(spec);
+
+        // Do the same for the other tabs
+        intent = new Intent().setClass(this, ViewTagsActivity.class);
+        spec = tabHost.newTabSpec("tags").setIndicator("Tags",
+                              res.getDrawable(R.drawable.ic_tab_artists))
+                          .setContent(intent);
+        tabHost.addTab(spec);
+
+
+        tabHost.setCurrentTab(1);
+        /*
         photoManager = new PhotoManager(this);
+        */
         
         /**TODO
          * map objects created as variables to real objects in the XML R.layout.main
          */
-        
+        /*
         newAlbumButton.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View v) {
 				setResult(RESULT_OK);
@@ -42,6 +74,7 @@ public class Skindex extends Skindactivity
 				takePicture();
 			}
 		});
+		*/
         
         /**TODO
          * we should add all the other listeners here.
@@ -57,7 +90,7 @@ public class Skindex extends Skindactivity
      * use selectAllAlbums and selectAllTags to get the lists you need to
      * populate the screen
      */
-    @Override
+    //@Override
     public void update(Object model) {
         //update whatever screen is up.
     }
--- src/com/cs301w01/meatload/activities/ViewTagsActivity.java
+++ src/com/cs301w01/meatload/activities/ViewTagsActivity.java
@@ -1,6 +1,8 @@
 package com.cs301w01.meatload.activities;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
 
 
 public class ViewTagsActivity extends Skindactivity {
@@ -9,4 +11,13 @@ public class ViewTagsActivity extends Skindactivity {
     public void update(Object model) {
         //To change body of implemented methods use File | Settings | File Templates.
     }
+    
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        TextView textview = new TextView(this);
+        textview.setText("This is the Tags tab");
+        setContentView(textview);
+    }
+
 }
