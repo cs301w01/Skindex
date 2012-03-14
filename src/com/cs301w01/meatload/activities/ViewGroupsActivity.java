fb1fa9f70dc658fe35618aa008c73a0b4751786e
Joel Burford <joelburford@gmail.com>
2012-03-13 19:46:05 -0600

    more menu stuff

--- src/com/cs301w01/meatload/activities/ViewGroupsActivity.java
+++ src/com/cs301w01/meatload/activities/ViewGroupsActivity.java
@@ -2,10 +2,13 @@ package com.cs301w01.meatload.activities;
 
 import java.util.Collection;
 
+import com.cs301w01.meatload.R;
 import com.cs301w01.meatload.controllers.PhotoManager;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 
@@ -19,13 +22,28 @@ public class ViewGroupsActivity extends Skindactivity {
     
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        
+        //Code taken from here: http://www.vogella.de/articles/AndroidListView/article.html
+        
+        ListView listView = new ListView(this);
+        String[] values = new String[] { "Android", "iPhone", "WindowsMobile",
+        	"Blackberry", "WebOS", "Ubuntu", "Windows7", "Max OS X",
+        	"Linux", "OS/2" };
 
-        TextView textview = new TextView(this);
-        textview.setText("This is the Albums tab");
-        setContentView(textview);
+        // First paramenter - Context
+        // Second parameter - Layout for the row
+        // Third parameter - ID of the View to which the data is written
+        // Forth - the Array of data
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+        	android.R.layout.simple_list_item_1, android.R.id.text1, values);
+
+        // Assign adapter to ListView
+        listView.setAdapter(adapter);
+        setContentView(listView);
+       
         
         photoManager = new PhotoManager(this);
-        Collection<String> aNames = photoManager.getAlbumNames();
+        //Collection<String> aNames = photoManager.getAlbumNames();
     }
 
 }
