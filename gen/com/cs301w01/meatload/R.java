2962b253a9ad4081c7b72bafefd15aebdab76aed
Joel Burford <joelburford@gmail.com>
2012-03-14 15:59:55 -0600

    deleting

--- gen/com/cs301w01/meatload/R.java
+++ gen/com/cs301w01/meatload/R.java
@@ -11,13 +11,10 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_tab_artists=0x7f020000;
-        public static final int ic_tab_artists_grey=0x7f020001;
-        public static final int ic_tab_artists_white=0x7f020002;
-        public static final int icon=0x7f020003;
-        public static final int tab_icon_grey=0x7f020004;
-        public static final int tab_icon_white=0x7f020005;
-        public static final int tab_main=0x7f020006;
+        public static final int icon=0x7f020000;
+        public static final int tab_icon_grey=0x7f020001;
+        public static final int tab_icon_white=0x7f020002;
+        public static final int tab_main=0x7f020003;
     }
     public static final class id {
         public static final int button1=0x7f050005;
