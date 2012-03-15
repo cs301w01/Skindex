07768877efd3244d9f7693aa8bea51a77b3add35
Blake Bouchard <thatsblake@gmail.com>
2012-03-15 16:40:26 -0600

    Mucked with EditAlbumActivity, created layout, changed Photo to Photomanager in EditPictureActivity

--- src/com/cs301w01/meatload/activities/EditAlbumActivity.java
+++ src/com/cs301w01/meatload/activities/EditAlbumActivity.java
@@ -1,12 +1,19 @@
 package com.cs301w01.meatload.activities;
 
-import android.app.Activity;
+import com.cs301w01.meatload.R;
 
+import android.os.Bundle;
 
 public class EditAlbumActivity extends Skindactivity {
 
-    //@Override
+    @Override
     public void update(Object model) {
         //To change body of implemented methods use File | Settings | File Templates.
+    }
+    
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+    	super.onCreate(savedInstanceState);
+    	setContentView(R.layout.edit_album);
     }
 }
--- src/com/cs301w01/meatload/activities/EditPictureActivity.java
+++ src/com/cs301w01/meatload/activities/EditPictureActivity.java
@@ -1,7 +1,7 @@
 package com.cs301w01.meatload.activities;
 
 import com.cs301w01.meatload.R;
-import com.cs301w01.meatload.model.Photo;
+import com.cs301w01.meatload.controllers.PhotoManager;
 
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
@@ -19,7 +19,7 @@ import android.widget.TextView;
  */
 public class EditPictureActivity extends Skindactivity {
 
-	Photo picture;
+	PhotoManager pictureManager;
 	
 	@Override
 	public void update(Object model) {
@@ -32,19 +32,19 @@ public class EditPictureActivity extends Skindactivity {
 		setContentView(R.layout.edit_picture);
 		
 		// Get picture object from Intent's extras bundle
-		picture = (Photo) getIntent().getExtras().get("Picture");
+		pictureManager = (PhotoManager) getIntent().getExtras().getSerializable("Manager");
 		
 		// Set pictureView to path provided by Picture object
 		ImageView pictureView = (ImageView) findViewById(R.id.pictureView);
-		pictureView.setImageDrawable(Drawable.createFromPath(picture.getPath()));
+		pictureView.setImageDrawable(Drawable.createFromPath(pictureManager.getPath()));
 		
 		// Set dateView to toString representation of Date in Picture object
 		TextView dateView = (TextView) findViewById(R.id.dateView);
-		dateView.setText(picture.getDate().toString());
+		dateView.setText(pictureManager.getDate().toString());
 		
 		// Set albumView to string representation of Album in Picture object
 		TextView albumView = (TextView) findViewById(R.id.albumView);
-		albumView.setText(picture.getAlbumName());
+		albumView.setText(pictureManager.getAlbumName());
 		
 		Button changeAlbumButton = (Button) findViewById(R.id.changeAlbumButton);
 		// TODO: Add Change Album functionality to EditPicture
