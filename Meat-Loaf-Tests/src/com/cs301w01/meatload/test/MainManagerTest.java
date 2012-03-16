a58ec596606dba62d692e3f74c395898fa200786
Joel Burford <joelburford@gmail.com>
2012-03-15 21:39:30 -0600

    adding junit test project

---
+++ Meat-Loaf-Tests/src/com/cs301w01/meatload/test/MainManagerTest.java
@@ -0,0 +1,28 @@
+package com.cs301w01.meatload.test;
+
+import android.test.AndroidTestCase;
+import com.cs301w01.meatload.*;
+import com.cs301w01.meatload.controllers.MainManager;
+
+public class MainManagerTest extends AndroidTestCase {
+	MainManager mClassToTest;
+	int mArg1;
+	int mArg2;
+ 
+	protected void setUp() throws Exception {
+		mClassToTest=new MainManager();
+		mArg1=6;
+		mArg2=3;
+		super.setUp();
+	}
+ 
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+ 
+	public void testAdd(){
+		assertEquals(9,9);
+		assertEquals(5,4);
+		//assertEquals(9,mClassToTest.add(mArg1,mArg2));
+	}
+}
