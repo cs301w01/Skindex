57a41c9a1877e526c561428524f31bb7ff5e92ae
deredowl <deredowl@hotmail.com>
2012-03-28 11:53:43 -0600

    Create base framework and skeleton classes for user system if we want to implement it.

---
+++ Meat-Loaf/src/com/cs301w01/meatload/authentication/Controllers/UserManager.java
@@ -0,0 +1,11 @@
+package com.cs301w01.meatload.authentication.Controllers;
+
+import com.cs301w01.meatload.controllers.FController;
+
+/**
+ * This class handles all the intermediate steps between the db and the GUI/activity.
+ */
+public class UserManager implements FController{
+
+
+}
---
+++ Meat-Loaf/src/com/cs301w01/meatload/authentication/activities/ViewPatientsActivity.java
@@ -0,0 +1,18 @@
+package com.cs301w01.meatload.authentication.activities;
+
+import com.cs301w01.meatload.activities.Skindactivity;
+
+/**
+ * This Acitivity is used by users who log in as a specialist. It allows them to confirm new patients, and view
+ * the albums of existing patients.
+ * @author Derek Dowling
+ */
+public class ViewPatientsActivity extends Skindactivity {
+
+    //TODO: implement all of it
+
+    @Override
+    public void update(Object model) {
+
+    }
+}
