9363f1acd8c39abbee910a92b413db0501c4d168
Joel Burford <joelburford@gmail.com>
2012-04-04 17:02:49 -0600

    tests

--- Meat-Loaf-Tests/src/com/cs301w01/meatload/test/ActivityTests/DatabaseTest.java
+++ Meat-Loaf-Tests/src/com/cs301w01/meatload/test/ActivityTests/DatabaseTest.java
@@ -1,7 +1,16 @@
 package com.cs301w01.meatload.test.ActivityTests;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 import com.cs301w01.meatload.activities.ViewAlbumsActivity;
+import com.cs301w01.meatload.model.Album;
+import com.cs301w01.meatload.model.PictureGenerator;
 import com.cs301w01.meatload.model.SQLiteDBManager;
+import com.cs301w01.meatload.model.Tag;
+import com.cs301w01.meatload.controllers.MainManager;
+import com.cs301w01.meatload.controllers.PictureManager;
+import com.cs301w01.meatload.model.querygenerators.AlbumQueryGenerator;
 
 import android.content.Context;
 import android.test.ActivityInstrumentationTestCase2;
@@ -9,7 +18,8 @@ import android.test.ActivityInstrumentationTestCase2;
 public class DatabaseTest extends ActivityInstrumentationTestCase2<ViewAlbumsActivity> {
     private Context mContext;
     private ViewAlbumsActivity mActivity;
-    boolean firstRun = true;
+    private MainManager mainMan;
+    private PictureManager picMan;
     
 	public DatabaseTest(){
 		super("com.cs301w01.meatload", ViewAlbumsActivity.class);
@@ -22,13 +32,9 @@ public class DatabaseTest extends ActivityInstrumentationTestCase2<ViewAlbumsAct
         mActivity = getActivity();
         mContext = mActivity.getBaseContext();
         
-        if(firstRun){
-        	firstRun = false;
-        	SQLiteDBManager db = new SQLiteDBManager(mContext);
-            db.resetDB();
-            db.close();
-        }
-
+		mainMan = new MainManager();
+		mainMan.setContext(mContext);
+		picMan = new PictureManager(mContext);
     }
     
     @Override
@@ -41,40 +47,91 @@ public class DatabaseTest extends ActivityInstrumentationTestCase2<ViewAlbumsAct
 
     }
     
-    public void testAddAlbum(){
-    	assert(1==1);
+    public void test1stResetDB(){
+
+        	SQLiteDBManager db = new SQLiteDBManager(mContext);
+            db.resetDB();
+            db.close();
+            
+            assertTrue(mainMan.getAllAlbums().size() == 0);
     }
     
-    public void testDeleteAlbum(){
-    	assert(1==1);
+    public void testAddAlbum(){
+    	
+    	int oldNumAlbums = mainMan.getAllAlbums().size();
+    	int newNumAlbums;
+    	
+    	mainMan.addAlbum("Album 1", new ArrayList<String>());
+    	mainMan.addAlbum("Album 2", new ArrayList<String>());
+    	mainMan.addAlbum("Album 3", new ArrayList<String>());
+    	mainMan.addAlbum("Album 4", new ArrayList<String>());
+		
+		newNumAlbums = mainMan.getAllAlbums().size();
+		
+		assertTrue(newNumAlbums == oldNumAlbums + 4);
+    }
+    
+    public void testDeleteAlbum(){    	
+    	assertTrue(1==1);
     }
     
     public void testUpdateAlbum(){
-    	assert(1==1);
+    	assertTrue(1==1);
     }
     
-    public void testAddPicture(){
-    	assert(1==1);
+    public void testAddPicture1(){
+    	Album testAlbum;
+    	int oldNumPics;
+    	int newNumPics;
+    	PictureGenerator picGen = new PictureGenerator();
+    	
+    	testAlbum = mainMan.getAlbumByName("Album 1");
+    	oldNumPics = testAlbum.getPictureCount();
+
+    	picMan.takePicture(mContext.getFilesDir(), picGen.generatePicture(), "Album 1");
+    	picMan.takePicture(mContext.getFilesDir(), picGen.generatePicture(), "Album 1");
+    	picMan.takePicture(mContext.getFilesDir(), picGen.generatePicture(), "Album 1");
+    	
+    	testAlbum = mainMan.getAlbumByName("Album 1");
+    	newNumPics = testAlbum.getPictureCount();
+    	
+    	assertTrue(newNumPics == oldNumPics + 3);
+    }
+    
+    public void testAddPicture2(){
+    	int oldNumPics;
+    	int newNumPics;
+    	PictureGenerator picGen = new PictureGenerator();
+    	
+    	oldNumPics = mainMan.getPictureCount();
+
+    	picMan.takePicture(mContext.getFilesDir(), picGen.generatePicture(), "Album 2");
+    	picMan.takePicture(mContext.getFilesDir(), picGen.generatePicture(), "Album 3");
+    	picMan.takePicture(mContext.getFilesDir(), picGen.generatePicture(), "Album 3");
+    	
+    	newNumPics = mainMan.getPictureCount();
+    	
+    	assertTrue(newNumPics == oldNumPics + 3);
     }
     
     public void testDeletePicture(){
-    	assert(1==1);
+    	assertTrue(1==1);
     }
     
     public void testUpdatePicture(){
-    	assert(1==1);
+    	assertTrue(1==1);
     }
     
     public void testAddTags(){
-    	assert(1==1);
+    	assertTrue(1==1);
     }
     
     public void testDeleteTags(){
-    	assert(1==1);
+    	assertTrue(1==1);
     }
     
     public void testUpdateTags(){
-    	assert(1==1);
+    	assertTrue(1==1);
     }
 	
 	
