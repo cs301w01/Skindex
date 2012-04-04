b78ad5492a882eb7f8aad842e6c26506443021e5
Joel Burford <joelburford@gmail.com>
2012-04-03 18:05:31 -0600

    Comments

--- Meat-Loaf/src/com/cs301w01/meatload/model/Album.java
+++ Meat-Loaf/src/com/cs301w01/meatload/model/Album.java
@@ -7,19 +7,24 @@ import java.util.Collection;
 import java.util.Date;
 
 /**
- * 
+ * Stores temporary values representing an Album to facilitate reading and writing album
+ * details from the database.
  */
 public class Album implements Serializable {
     
-    /**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 	private String name;
     private int pictureCount;
     private long id;
     private Date date;
     
+    /**
+     * Constructor, creates an Album object with the given name, # of pictures, id, and date.
+     * @param name
+     * @param pictureCount
+     * @param id
+     * @param date
+     */
     public Album(String name, int pictureCount, long id, Date date) {
     	this.name = name;
     	this.pictureCount = pictureCount;
@@ -28,6 +33,9 @@ public class Album implements Serializable {
     	
     }
     
+    /**
+     * Constructor, creates an Album object with a given Name, # of pictures, and id
+     */
     public Album(String name, int pictureCount, long id) {
     	this.name = name;
     	this.pictureCount = pictureCount;
@@ -35,30 +43,52 @@ public class Album implements Serializable {
     	
     }
     
+    /**
+     * Constructor, creates an Album object with a given Name
+     * @param name
+     */
     public Album(String name){
     	this.name = name;
     	pictureCount = 0;
     	id = -1;
     }
     
+    /**
+     * Returns the current Album's Name
+     * @return String
+     */
     public String getName() {
     	return name;
     }
     
+    /**
+     * Returns the current Album's picture count
+     * @return int
+     */
     public int getPictureCount() {
     	return pictureCount;
     }
     
+    /**
+     * Returns the current Album's ID
+     * @return long
+     */
     public long getID() {
         return id;
     }
     
+    /**
+     * Returns the current Album's Date
+     * @return Date
+     */
     public Date getDate() {
     	return Calendar.getInstance().getTime();
-    	
-    	//return date;
     }
     
+    /**
+     * Returns a formatted string representing the current Album.
+     * @return String
+     */
     @Override
     public String toString() {
 		return getName();
