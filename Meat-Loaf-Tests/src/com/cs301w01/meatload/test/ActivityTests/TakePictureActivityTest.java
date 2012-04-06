1c55fca58df089ae67a56cf0780ecd06b7a5b628
Blake Bouchard <thatsblake@gmail.com>
2012-04-06 02:08:53 -0600

    Wrote two tests in TakePictureActivity

--- Meat-Loaf-Tests/src/com/cs301w01/meatload/test/ActivityTests/TakePictureActivityTest.java
+++ Meat-Loaf-Tests/src/com/cs301w01/meatload/test/ActivityTests/TakePictureActivityTest.java
@@ -1,55 +1,69 @@
 package com.cs301w01.meatload.test.ActivityTests;
 
+import com.cs301w01.meatload.R;
 import com.cs301w01.meatload.activities.TakePictureActivity;
-import com.cs301w01.meatload.model.SQLiteDBManager;
+import com.cs301w01.meatload.controllers.MainManager;
 
 import android.app.Instrumentation;
 import android.content.Context;
 import android.test.ActivityInstrumentationTestCase2;
+import android.widget.Button;
+import android.widget.ImageView;
 
-public class TakePictureActivityTest extends
-		ActivityInstrumentationTestCase2<TakePictureActivity> {
-    private Instrumentation mInstrumentation;
-    private Context mContext;
-    private TakePictureActivity mActivity;
-    
-	public TakePictureActivityTest(){
+public class TakePictureActivityTest extends ActivityInstrumentationTestCase2<TakePictureActivity> {
+	private Instrumentation mInstrumentation;
+	private Context mContext;
+	private TakePictureActivity mActivity;
+
+	public TakePictureActivityTest() {
 		super("com.cs301w01.meatload", TakePictureActivity.class);
 	}
-	
-    @Override
-    protected void setUp() throws Exception {
-    	super.setUp();
-        
-        mInstrumentation = getInstrumentation();
-        mContext = mInstrumentation.getContext();
-        mActivity = getActivity();
-    }
-    
-    @Override
-    protected void tearDown() throws Exception {
-        super.tearDown();    
-        
-        if (mActivity != null) {
-            mActivity.finish();
-        }
 
-    }
-    
-    public void testNumberOfComparePicturesLoaded(){
-    	assertTrue(1 == 1);
-    }
-    
-    public void testGeneratePic(){
-    	//ensure new photo is populated
-    }
-    
-    public void testTakePicCancel(){
-    	//press cancel in the dialog and ensure database does not change
-    }
-    
-    public void testTakePicOK(){
-    	//capture Intent and ensure picture saved validly
-    }
+	@Override
+	protected void setUp() throws Exception {
+		super.setUp();
+
+		mInstrumentation = getInstrumentation();
+		mContext = mInstrumentation.getContext();
+		mActivity = getActivity();
+	}
+
+	@Override
+	protected void tearDown() throws Exception {
+		super.tearDown();
+
+		if (mActivity != null) {
+			mActivity.finish();
+		}
+
+	}
+
+	public void testNumberOfComparePicturesLoaded() {
+		assertTrue(1 == 1);
+	}
+
+	public void testGeneratePic() {
+		// ensure new photo is populated
+		ImageView imageView = (ImageView) mActivity.findViewById(R.id.imgDisplay);
+		assertNotNull(imageView.getDrawable());
+	}
+
+	public void testTakePicCancel() {
+		// press cancel in the dialog and ensure database does not change
+		MainManager mainManager = new MainManager(mContext);
+		int initialPictureCount = mainManager.getPictureCount();
+		
+		Button cancelButton = (Button) mActivity.findViewById(R.id.cancelDialogButton);
+		cancelButton.requestFocus();
+		cancelButton.performClick();
+		
+		int finalPictureCount = mainManager.getPictureCount();
+		assertEquals(initialPictureCount, finalPictureCount);
+	}
+
+	public void testTakePicOK() {
+		// capture Intent and ensure picture saved validly
+		
+	}
 
 }
